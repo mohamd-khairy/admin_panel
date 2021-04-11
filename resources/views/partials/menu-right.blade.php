5965070cf77d6cba70fea05aedc073433a5d36dd
mohamed-khairy <m.khairy@evntoo.com>
2021-04-11 14:15:42 +0200

    fix

--- resources/views/partials/menu-right.blade.php
+++ resources/views/partials/menu-right.blade.php
@@ -3,23 +3,23 @@
     <div class="main_sidebar_wrapper">
         <ul>
             @if(\App\Models\Page::count())
-                @foreach(\App\Models\Page::with('subPage')->where('parent',0)->orderBy('order','asc')->get() as $page)
-                <li><span class="{{$page->icon}}"></span>
-                    <a href="{{count($page->subPage) > 0 ? '#' : url('/admin/'.$page->url)}}" style="{{\Request::segment(2) == $page->url || (\Request::segment(2) == null && $page->url == '/') ? 'color:red' : ''}}" >{{ $page->name }} </a>
-                    @if(count($page->subPage) > 0)
-                    <ul class="drop_main_menu">
-                        @foreach($page->subPage as $subPage)
-                        <li><a href="{{url('/admin/'.$subPage->url)}}">{{ $subPage->name }}</a></li>
-                        @endforeach
-                    </ul>
-                    @endif
-                </li>
-                @endforeach
+            @foreach(\App\Models\Page::with('subPage')->where('parent',0)->orderBy('order','asc')->get() as $page)
+            <li><span class="{{$page->icon}}"></span>
+                <a href="{{count($page->subPage) > 0 ? '#' : url('/admin/'.$page->url)}}" style="{{\Request::segment(2) == $page->url || \Request::segment(2) == explode('/',$page->url)[0] || (\Request::segment(2) == null && $page->url == '/') ? 'color:red' : ''}}">{{ $page->name }} </a>
+                @if(count($page->subPage) > 0)
+                <ul class="drop_main_menu">
+                    @foreach($page->subPage as $subPage)
+                    <li><a href="{{url('/admin/'.$subPage->url)}}">{{ $subPage->name }}</a></li>
+                    @endforeach
+                </ul>
+                @endif
+            </li>
+            @endforeach
             @endif
             <li class="c-sidebar-nav-item">
-            <span class="glyphicon glyphicon-log-out"></span>
+                <span class="glyphicon glyphicon-log-out"></span>
 
-                <a href="{{ url('/admin/logout')}}"  onclick="event.preventDefault(); document.getElementById('logoutform').submit();">
+                <a href="{{ url('/admin/logout')}}" onclick="event.preventDefault(); document.getElementById('logoutform').submit();">
                     {{__('admin.pages.logout')}}
                 </a>
             </li>
